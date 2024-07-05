a7ee4dcd534f101c79fac691cf55cf4976604eaa
Aleksander Grande <aleksander.grande@eidsiva.no>
2024-07-05 19:52:07 +0200

    Fikser

--- Crunch-E.ino
+++ Crunch-E.ino
@@ -1,6 +1,5 @@
-#include <Arduino.h>
-
 #include "LedManager.h"
+#include <esp_task_wdt.h>  
 LedManager ledManager = LedManager(1, 2, 3, 4);
 
 #include "InputManager.h"
@@ -22,33 +21,40 @@ char keys[ROWS][COLS] = {
   { 'A', 'B', 'C', 'D' }
 };
 
-byte rowPins[ROWS] = { 11, 10, 9, 8 };     //connect to the row pinouts of the keypad
-byte colPins[COLS] = { 12, 13, 14, 15 };  //connect to the column pinouts of the keypad
+byte rowPins[ROWS] = { 13, 12, 14, 27 };     //connect to the row pinouts of the keypad
+byte colPins[COLS] = { 26, 25, 33, 32 };  //connect to the column pinouts of the keypad
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
-
+#define WDT_TIMEOUT   3   
 float lastMillis;
 char oldChar;
+
 //i2s sound
 #include <I2S.h>
 const int sampleRate = 22000;  // sample rate in Hz
 
 void setup() {
+
   lastMillis = millis();
- 
+    Serial.println("Before I2S!");
+
   //setup I2S pins
-  I2S.setDataPin(7);
-  I2S.setSckPin(6);//blc
+  I2S.setDataPin(19);
+  I2S.setSckPin(18);//blc
   I2S.setFsPin(5);//lrc
+  Serial.println("After I2S!");
 
   if (!I2S.begin(I2S_PHILIPS_MODE, sampleRate, 16)) {
     Serial.println("Failed to initialize I2S!");
-    while (1) ;
-     
+    while (true);  // Halt execution
   }
-
 }
 
 void loop() {
+
+  
+  // Debug prints to track execution
+
+
   inputManager.UpdateInput(keypad.getKey());
   char note = inputManager.note;
   char trackCommand = inputManager.trackCommand;
@@ -64,8 +70,20 @@ void loop() {
   }
 
   int sample = tracker.UpdateTracker();
-  sample /= 5;
- 
+
+
+
+  // Loudness adjustment
+  if (abs(sample) > 4000) {
+    int rem = abs(sample) - 4000;
+    if (sample > 0) {
+      sample = 4000 + (rem / 2);
+    } else {
+      sample = -4000 - (rem / 2);
+    }
+  }
+  sample /= 4;
+
   I2S.write(sample);
   I2S.write(sample);
 
@@ -73,8 +91,10 @@ void loop() {
   if (tempoBlink > 0)
     ledManager.SetLit(tempoBlink, tracker.selectedTrack);
 
-  ledManager.SetPattern(tracker.allPatternPlay,tracker.currentPattern);
+  ledManager.SetPattern(tracker.allPatternPlay, tracker.currentPattern);
   ledManager.UpdateLed();
   inputManager.EndFrame();
-}
 
+  // Debug print to ensure loop completion
+
+}
